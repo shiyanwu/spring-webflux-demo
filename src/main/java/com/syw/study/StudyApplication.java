c92a11f3e6bcacc607110b34d91d58bce0486a42
shiyanwu <yanuwu.shi@ehking.com>
2018-06-09 09:10:34 +0800

    去年多余的

--- src/main/java/com/syw/study/StudyApplication.java
+++ src/main/java/com/syw/study/StudyApplication.java
@@ -2,8 +2,6 @@ package com.syw.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 /**
  * @author shiyanwu
